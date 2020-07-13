91051e7e7f7aec0e8bebd205c37ea3dae53200ee
Thomas Morita <>
2020-07-13 11:33:04 -0400

    added house of reps

--- src/App.js
+++ src/App.js
@@ -1,101 +1,170 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { CongressmanCard } from './components/congressman-card';
-import { Button, Box, Text, Heading } from 'rebass';
+import { Button, Box, Text, Heading, Flex } from 'rebass';
 import { Label, Input } from '@rebass/forms';
 import { ThemeProvider } from 'emotion-theming';
 import theme from '@rebass/preset';
 import './App.css';
-import {getState} from './utils'
+import { getState } from './utils';
 
 function App() {
-  const [ listOfCongressman, setListOfCongressman ] = useState([]);
-  const [userLocation, setUserLocation] = useState(null)
+	const [ allSenators, setAllSenators ] = useState([]);
+	const [ listOfSenators, setListOfSenators ] = useState([]);
+	const [ listOfReps, setListOfReps ] = useState([]);
+	const [ userLocation, setUserLocation ] = useState({ state: null, district: null });
+	const [ isLoading, setIsLoading ] = useState(false);
 
+	//********* ON MOUNT *********/
 	useEffect(() => {
 		if ('geolocation' in navigator) {
 			console.log('Available');
 		} else {
 			console.log('Not Available');
 		}
-  }, []);
-  
-  useEffect(()=>{
-    // IF USER LOCATION CHANGES, MAKE NEW CALL 
-    handleFetchSenators(userLocation)
-  }, [userLocation])
+		// get ALL senators on init
+		axios
+			.get('https://api.propublica.org/congress/v1/116/senate/members.json', {
+				headers: {
+					'X-API-Key': 'Gp5WuTAlck2YqCBBLPuHzIrvVtqgDMlALlUKAFDt'
+				}
+			})
+			.then((res) => setAllSenators(res.data.results[0].members))
+			.catch((error) => console.error(error));
+	}, []);
 
-  const handleFetchSenators = (st) => {
-    axios
-    .get(`https://api.propublica.org/congress/v1/members/senate/${st}/current.json`, {
-      headers: {
-        'X-API-Key': 'Gp5WuTAlck2YqCBBLPuHzIrvVtqgDMlALlUKAFDt'
-      }
-    })
-    .then((res) => {
-      console.log(res.data.results);
-      setListOfCongressman(res.data.results)
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  }
+	useEffect(
+		() => {
+      handleGetReps();
+      handleGetSenators(userLocation.state)
+		},
+		[ userLocation ]
+	);
 
-  // TODO 
+	const handleGetSenators = (st) => {
+		setIsLoading(true);
+		const senators = allSenators.filter((senator) => senator.state === st);
+		setListOfSenators(senators);
+		console.log({ senators });
+		setIsLoading(false);
+	};
+
+	const handleGetReps = () => {
+		if (userLocation.district) {
+			let districtParam = () => {
+				if ([ 'DC', 'AK', 'DE', 'MT', 'ND', 'SD', 'VT', 'WY' ].includes(userLocation.state)) return 1;
+				else return userLocation.district.district_number;
+			};
+			axios
+				.get(
+					` https://api.propublica.org/congress/v1/members/house/${userLocation.state}/${districtParam()}/current.json`,
+					{
+						headers: {
+							'X-API-Key': 'Gp5WuTAlck2YqCBBLPuHzIrvVtqgDMlALlUKAFDt'
+						}
+					}
+				)
+				.then((res) => {
+					setListOfReps(res.data.results);
+				})
+				.catch((err) => console.error(err));
+		}
+	};
+
+	const getUserLocationWithLatLng = (latlng) => {
+		axios
+			.get(
+				`https://api.geocod.io/v1.6/reverse?q=${latlng}&&fields=cd&api_key=d5cad54ac545cd2cb9d2f0c525df55a0c450fad`
+			)
+			.then((res) => {
+				const st = res.data.results[0].address_components['state'];
+				const districtData = res.data.results[0].fields.congressional_districts[0];
+				setUserLocation({ state: st, district: districtData });
+			})
+			.catch((err) => console.log(err));
+	};
+
 	const handleLookUpUserLocation = () => {
-    // GET USER LOCATION (LATITUDE/LONGITUDE)
-		navigator.geolocation.getCurrentPosition((position) => {
-			console.log({ position });
-			console.log('Latitude is :', position.coords.latitude);
-      console.log('Longitude is :', position.coords.longitude);
-      // const latlng = `${position.coords.latitude}, ${position.coords.longitude}`
-      const latlng = `36.0822, -94.1719`
-      axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng}&key=AIzaSyD5ZpUp3QDxo9YgwlZ7y-j-S8xOCe17XnA`)
-      .then((res) => {
-        console.log(res.data)
 
-      })
-      .catch((err) => {
-        console.error(err)
-      })
-    });
-    // HANDLE FETCH SENATOR
-    handleFetchSenators()
-  };
-  
-  const handleInput = (e) => {
-    let val = e.target.value
-    if(val.length === 5){
-      setUserLocation(getState(val))
-    }
-  }
+		let locationPayload = {};
+		setIsLoading(true);
+		// GET USER LOCATION (LATITUDE/LONGITUDE)
+		navigator.geolocation.getCurrentPosition((position) => {
+			const latlng = `${position.coords.latitude}, ${position.coords.longitude}`
+			// const latlng = `36.0822, -94.1719`; // fay
+			getUserLocationWithLatLng(latlng);
+			axios
+				.get(
+					`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng}&key=AIzaSyD5ZpUp3QDxo9YgwlZ7y-j-S8xOCe17XnA`
+				)
+				.then((res) => {
+					// Add STATE location to look up Senators
+					locationPayload.state = res.data.plus_code.compound_code.split(',')[1].replace(/\s/g, '');
+					// HANDLE FETCH SENATOR
+					handleGetSenators(locationPayload.state);
+					handleGetReps();
+					setIsLoading(false);
+				})
+				.catch((err) => {
+					console.error(err);
+					setIsLoading(false);
+				});
+		});
+	};
+
+	const handleInput = (e) => {
+		let val = e.target.value;
+		if (val.length >= 5) {
+			setUserLocation({ state: getState(val) });
+		}
+		if (val.length === 0) {
+			setUserLocation({ state: null });
+		}
+	};
 
 	return (
 		<ThemeProvider theme={theme}>
-			<div className="App">
+			<div className="App" style={{ height: '100vh' }}>
+				<Flex>
+					<Box p={3} width={1 / 5} bg="#ef476f" />
+					<Box p={3} width={1 / 5} bg="#ffd166" />
+					<Box p={3} width={1 / 5} bg="#06d6a0" />
+					<Box p={3} width={1 / 5} bg="#118ab2" />
+					<Box p={3} width={1 / 5} bg="#073b4c" />
+				</Flex>
 				<Box sx={{ p: 5 }}>
 					<Heading fontSize={[ 5, 6, 7 ]} color="black">
-						Call <br />My <br />Congressman
+						CALL <br />MY <br />CONGRESSMAN
 					</Heading>
 					<br />
 					<br />
 
 					<Box sx={{ mb: 3 }}>
 						<Label htmlFor="zipcode">SEARCH BY ZIPCODE</Label>
-						<Input id="email" name="email" type="email" placeholder="20003" onChange={handleInput} />
+						<Input id="email" name="email" type="email" placeholder="72701..." onChange={handleInput} />
 					</Box>
 					<Text>- OR -</Text>
 					<Button sx={{ mt: 3 }} onClick={handleLookUpUserLocation}>
 						Use my location
 					</Button>
 				</Box>
-      <Box sx={{margin: 'auto'}}>
-      {
-          listOfCongressman && listOfCongressman.map((person) => {
-            return <CongressmanCard  name={person.name} party={person.party}/>
-          })
-        }
-      </Box>
+				<Box sx={{ margin: 'auto' }}>
+					{isLoading ? (
+						'loading...'
+					) : (
+						(listOfSenators || listOfReps) &&
+						[ ...listOfSenators, ...listOfReps ].map((person) => {
+							return (
+								<CongressmanCard
+									phone={person.phone}
+									lastName={person.last_name}
+									firstName={person.first_name}
+									party={person.party}
+								/>
+							);
+						})
+					)}
+				</Box>
 			</div>
 		</ThemeProvider>
 	);
--- src/components/congressman-card.js
+++ src/components/congressman-card.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Button, Box, Text, Card, Heading } from 'rebass';
 
-export const CongressmanCard = ({ name, party }) => {
+export const CongressmanCard = ({ lastName, firstName, party, phone }) => {
 	return (
-		<Card width={256} sx={{ margin: 'auto' }}>
-			<Heading>{name} ({party})</Heading>
+		<Card width={256} sx={{ m: 'auto', mb: 2, bg: 'lightgrey', borderRadius: 5 }}>
+			<Heading sx={{ fontSize: 2 }}>
+				<a href={`tel:${phone}`} style={{ color: 'white', margin:'0 10px 0 10px' }}>
+					<img src="https://img.icons8.com/ios/50/000000/phone.png" style={{ height: 20, width: 20 }} />
+				</a>
+				{firstName} {lastName} ({party})
+			</Heading>
 		</Card>
 	);
 };
