be9c08f99f2b61aec88650c7ad65ed8d11e5bcbd
Thomas Morita <>
2020-07-13 13:27:53 -0400

    Updates

---
+++ precache-manifest.313dd0516617e8072736423cec284549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f729111984aab04a73c3fba514d59c",
+    "url": "/hey_congress/index.html"
+  },
+  {
+    "revision": "cd08871af74cd79ac03c",
+    "url": "/hey_congress/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2059af9e95dc424d6d69",
+    "url": "/hey_congress/static/js/2.a272681e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hey_congress/static/js/2.a272681e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd08871af74cd79ac03c",
+    "url": "/hey_congress/static/js/main.76700c83.chunk.js"
+  },
+  {
+    "revision": "7e89c5ee8f892c2a6a0d",
+    "url": "/hey_congress/static/js/runtime-main.55e07bce.js"
+  }
+]);
